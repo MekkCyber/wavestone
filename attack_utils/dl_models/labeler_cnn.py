cf82251949b7bafd7248fde3c2d73755f3ed9118
MekkCyber <mekk.cyber@gmail.com>
2023-12-14 11:17:18 +0100

    new model

--- attack_utils/dl_models/labeler_cnn.py
+++ attack_utils/dl_models/labeler_cnn.py
@@ -9,6 +9,9 @@ seed = 42
 np.random.seed(seed)
 tf.random.set_seed(seed)
 
+TRAIN_SPLIT = 0.6
+VAL_SPLIT = 0.8
+
 #choix base de données
 def get_dataset_tf(batch_size=128) : 
     (ds_train, ds_val, ds_test), ds_info = tfds.load(
@@ -38,33 +41,42 @@ def get_dataset_tf(batch_size=128) :
     ds_test = ds_test.cache().batch(32).prefetch(tf.data.AUTOTUNE)
     return ds_train, ds_val, ds_test
 
-def get_dataset(batch_size=128) : 
-    (ds_train, ds_val, ds_test), ds_info = tfds.load(
-        'mnist',
-        split=['train[10%:70%]','train[70%:]', 'test'],
-        shuffle_files=True,
-        as_supervised=True,
-        with_info=True,
-    )
 
+
+def get_dataset_keras(batch_size=128) : 
+    mnist = tf.keras.datasets.mnist
+    (train_images_raw, train_labels_raw), (test_images, test_labels) = mnist.load_data()
+
+    train_images = train_images_raw[int(len(train_images_raw)*0.1):int(len(train_images_raw)*TRAIN_SPLIT)]
+    train_labels = train_labels_raw[int(len(train_labels_raw)*0.1):int(len(train_labels_raw)*TRAIN_SPLIT)]
+
+    val_images = train_images_raw[int(len(train_images_raw)*TRAIN_SPLIT):int(len(train_images_raw)*VAL_SPLIT)]
+    val_labels = train_labels_raw[int(len(train_labels_raw)*TRAIN_SPLIT):int(len(train_labels_raw)*VAL_SPLIT)]
+    
     def normalize_img(image, label):
         return tf.cast(image, tf.float32) / 255., label
 
-    ds_train = ds_train.map(
-        normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
+    ds_train = tf.data.Dataset.from_tensor_slices((train_images, train_labels))
+    ds_train = ds_train.map(normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
     ds_train = ds_train.cache()
+    ds_train = ds_train.shuffle(buffer_size=10000)
     ds_train = ds_train.batch(batch_size)
     ds_train = ds_train.prefetch(tf.data.AUTOTUNE)
 
-    ds_val = ds_val.map(
-        normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
+    ds_val = tf.data.Dataset.from_tensor_slices((val_images, val_labels))
+    ds_val = ds_val.map(normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
     ds_val = ds_val.cache()
+    ds_val = ds_val.shuffle(buffer_size=10000)
     ds_val = ds_val.batch(batch_size)
     ds_val = ds_val.prefetch(tf.data.AUTOTUNE)
 
+    ds_test = tf.data.Dataset.from_tensor_slices((test_images, test_labels))
     ds_test = ds_test.map(normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
     ds_test = ds_test.cache().batch(32).prefetch(tf.data.AUTOTUNE)
+
     return ds_train, ds_val, ds_test
+
+
 
 
 
@@ -97,9 +109,7 @@ def create_model() :
 
 
 def train(model_conv, ds_train, ds_val, batch_size=128, epochs=20) : 
-    checkpoint_path = "checkpoints/labeler/training_1/cp-{epoch:02d}.ckpt"
-    n_batches = len(ds_train) // batch_size
-
+    checkpoint_path = "checkpoints/labeler/training_2/cp-{epoch:02d}.ckpt"
     cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
                                                     save_weights_only=True,
                                                     save_best_only=True,
@@ -114,6 +124,6 @@ def train(model_conv, ds_train, ds_val, batch_size=128, epochs=20) :
 
 
 if __name__ == "__main__" : 
-  ds_train, ds_val, ds_test = get_dataset()
+  ds_train, ds_val, ds_test = get_dataset_keras()
   model = create_model()
   train(model, ds_train, ds_val)
--- attack_utils/get_labeler_from_ckpt.py
+++ attack_utils/get_labeler_from_ckpt.py
@@ -3,10 +3,10 @@ from dl_models import labeler_cnn
 def get_labeler_from_ckpt(checkpoint_path=None) : 
     model = labeler_cnn.create_model()
     if checkpoint_path is None : 
-        checkpoint_path = "checkpoints/labeler/training_1/cp-19.ckpt"
+        checkpoint_path = "checkpoints/labeler/training_2/cp-08.ckpt"
 
     model.load_weights(checkpoint_path).expect_partial()
-    _, _, ds_test = labeler_cnn.get_dataset()
+    _, _, ds_test = labeler_cnn.get_dataset_keras()
     loss, acc = model.evaluate(ds_test, verbose=2)
     print("Restored model, accuracy: {:5.2f}%".format(100 * acc))
 
