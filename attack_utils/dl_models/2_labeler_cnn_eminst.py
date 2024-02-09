1e01393e844bc9de1328e25930ff07ad0ce2d5e0
Adri1P <adrien.peccenini@student-cs.fr>
2024-02-09 16:12:30 +0100

    modele emnist avec byclass

--- attack_utils/dl_models/2_labeler_cnn_eminst.py
+++ attack_utils/dl_models/2_labeler_cnn_eminst.py
@@ -29,38 +29,74 @@ from tensorflow.keras import callbacks
 from tensorflow.keras.callbacks import EarlyStopping
 #from tensorflow.keras import backend as K
 
-from emnist import list_datasets
-list_datasets()
-
-#I extract training samples from the EMNIST dataset.
-#The 'balanced' argument indicates the extraction of a balanced subset of data.
-#It includes an equal number of samples for each character class.
-
 from emnist import extract_training_samples
-x_train, y_train = extract_training_samples('balanced')
 from emnist import extract_test_samples
-x_test, y_test = extract_test_samples('balanced')
-#print(x_train.shape, y_train.shape, x_test.shape, y_test.shape)
+from tensorflow.keras.models import load_model
+# #print(x_train.shape, y_train.shape, x_test.shape, y_test.shape)
 
-# print("Number of training examples:", len(x_train))
-# print("Number of test examples:", len(x_test))
-# print("Image dimensions:", x_train[0].shape)
-# print("Number of classes:", len(np.unique(y_train)))
+# # print("Number of training examples:", len(x_train))
+# # print("Number of test examples:", len(x_test))
+# # print("Image dimensions:", x_train[0].shape)
+# # print("Number of classes:", len(np.unique(y_train)))
 
-#on normalise
+# #on normalise
 
-x_train=x_train/255
-x_test=x_test/255
+# x_train=x_train/255
+# x_test=x_test/255
 
-#array NumPy
-x_train = np.array(x_train).reshape(-1, 28, 28, 1)
-x_test =  np.array(x_test).reshape(-1, 28, 28, 1)
-print(x_train.shape)
-print(x_test.shape)
+# #array NumPy
+# x_train = np.array(x_train).reshape(-1, 28, 28, 1)
+# x_test =  np.array(x_test).reshape(-1, 28, 28, 1)
+# print(x_train.shape)
+# print(x_test.shape)
 
 #encodage one-hot : permet de représenter chaque classe comme vecteur binaire distinct
 #(0,0,1,0,...,0) par exemple est le vecteur binaire associé au label 3
-y_train = to_categorical(y_train)
+# y_train = to_categorical(y_train)
+
+TRAIN_SPLIT = 0.6
+VAL_SPLIT = 0.8
+
+def get_dataset_keras(batch_size=128) : 
+    
+    (train_images_raw, train_labels_raw), (test_images, test_labels) = extract_training_samples('byclass'), extract_test_samples('byclass')
+
+    train_images = train_images_raw[int(len(train_images_raw)*0.1):int(len(train_images_raw)*TRAIN_SPLIT)]
+    train_labels = train_labels_raw[int(len(train_labels_raw)*0.1):int(len(train_labels_raw)*TRAIN_SPLIT)]
+
+    val_images = train_images_raw[int(len(train_images_raw)*TRAIN_SPLIT):int(len(train_images_raw)*VAL_SPLIT)]
+    val_labels = train_labels_raw[int(len(train_labels_raw)*TRAIN_SPLIT):int(len(train_labels_raw)*VAL_SPLIT)]
+
+    #encodage one-hot : permet de représenter chaque classe comme vecteur binaire distinct
+    #(0,0,1,0,...,0) par exemple est le vecteur binaire associé au label 3
+    train_labels = to_categorical(train_labels)
+    val_labels = to_categorical(val_labels)
+    
+    def normalize_img(image, label):
+        return tf.cast(image, tf.float32) / 255., label
+
+    ds_train = tf.data.Dataset.from_tensor_slices((train_images, train_labels))
+    ds_train = ds_train.map(normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
+    ds_train = ds_train.cache()
+    ds_train = ds_train.shuffle(buffer_size=10000)
+    ds_train = ds_train.batch(batch_size)
+    ds_train = ds_train.prefetch(tf.data.AUTOTUNE)
+
+    ds_val = tf.data.Dataset.from_tensor_slices((val_images, val_labels))
+    ds_val = ds_val.map(normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
+    ds_val = ds_val.cache()
+    ds_val = ds_val.shuffle(buffer_size=10000)
+    ds_val = ds_val.batch(batch_size)
+    ds_val = ds_val.prefetch(tf.data.AUTOTUNE)
+
+    ds_test = tf.data.Dataset.from_tensor_slices((test_images, test_labels))
+    ds_test = ds_test.map(normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
+    ds_test = ds_test.cache().batch(32).prefetch(tf.data.AUTOTUNE)
+
+    return ds_train, ds_val, ds_test
+
+
+
 
 
 #CNN process
@@ -68,53 +104,88 @@ y_train = to_categorical(y_train)
 #K.clear_session()
 
 # Define the model using the functional approach
-cnn= Sequential()
-cnn.add(Conv2D(filters=32, kernel_size=(5,5), padding='same', activation='tanh', input_shape=(28, 28, 1)))
-cnn.add(MaxPooling2D(strides=2))
-cnn.add(Conv2D(filters=48, kernel_size=(5,5), padding='same', activation='tanh'))
-cnn.add(MaxPooling2D( strides=2))
-cnn.add(Conv2D(filters=64, kernel_size=(5,5), padding='same', activation='tanh'))
-cnn.add(Flatten())
-cnn.add(Dense(512, activation='tanh'))
-cnn.add(Dense(84, activation='tanh'))
-cnn.add(Dense(47, activation='softmax'))
+def create_model():
+    cnn= Sequential()
+    cnn.add(Conv2D(filters=32, kernel_size=(5,5), padding='same', activation='tanh', input_shape=(28, 28, 1)))
+    cnn.add(MaxPooling2D(strides=2))
+    cnn.add(Conv2D(filters=48, kernel_size=(5,5), padding='same', activation='tanh'))
+    cnn.add(MaxPooling2D( strides=2))
+    cnn.add(Conv2D(filters=64, kernel_size=(5,5), padding='same', activation='tanh'))
+    cnn.add(Flatten())
+    cnn.add(Dense(512, activation='tanh'))
+    cnn.add(Dense(84, activation='tanh'))
+    cnn.add(Dense(62, activation='softmax'))
 
-#optimazer
-opt=Adam(learning_rate=1e-4)
+    #optimazer
+    opt=Adam(learning_rate=1e-4)
 
-#Let's compile the model before training.
-cnn.compile(optimizer=opt,
+    cnn.compile(optimizer=opt,
                    loss='categorical_crossentropy',
                    metrics=['accuracy'])
 
-cnn.summary()
+    cnn.summary()
+    return cnn
+
+def get_model():
+    return  tf.keras.models.load_model("checkpoints/labeler_cnn_emnist/training_2/cp-{epoch:02d}.ckpt")
+
 
 #keras.utils.plot_model(cnn, "model.png", show_shapes=True)
+def train(model_conv, ds_train, ds_val, batch_size=128, epochs=2):
+    checkpoint_path = "checkpoints/labeler_cnn_emnist/training_2/cp-{epoch:02d}.ckpt"
+    # keras_callbacks = [
+    #     EarlyStopping(monitor='val_loss', patience=5, verbose=1, min_delta=0.001)
+    # ]
+    cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
+                                                    save_weights_only=True,
+                                                    save_best_only=True,
+                                                    verbose=1)
 
-keras_callbacks = [
-    EarlyStopping(monitor='val_loss', patience=5, verbose=1, min_delta=0.001)
-]
+    #enlever cllbacks=[cp_callback] pour ne plus overwright les poids une fois le modele entrainé
+    model_conv.fit(
+        ds_train,
+        epochs = epochs,
+        validation_data = ds_val,
+        callbacks=[cp_callback]
+    )
 
-start_time = time.time() 
-history = cnn.fit(x_train, y_train, epochs=1, batch_size=64, verbose=1,
-                    validation_split=0.2, callbacks=keras_callbacks)
+def visualisation(model, train, ds_test, ds_val, ds_train):
+    # Visu
 
-end_time = time.time() 
 
-#visualisation résultats
+    x_test_list = []
+    y_test_list = []
 
-# cols = 8
-# rows = 4
+    # Itérer sur le dataset et extraire les valeurs
+    for x, y in ds_test:
+        x_test_list.append(x.numpy())  # Convertir le tenseur TensorFlow en tableau NumPy
+        y_test_list.append(y.numpy())  # Convertir le tenseur TensorFlow en tableau NumPy
 
-# plt.figure(figsize=(cols, rows))
+    # Convertir les listes en tableaux NumPy
+    x_test = np.concatenate(x_test_list, axis=0)
+    y_test = np.concatenate(y_test_list, axis=0)
 
-# for image in x_test[1] :
-#     for label in y_test[1] :
-#         preds = model_conv.predict(image)
-#         preds = tf.nn.softmax(preds, axis=-1)
-#         for i in range(32) :
-#             plt.subplot(rows, cols, i + 1)
-#             plt.title(f"{label[i].numpy()}-{np.argmax(preds[i])}")
-#             plt.imshow(image[i])
-#             plt.axis('off')
-#     break
+    # Sélectionnez une image à partir de l'ensemble de test EMNIST
+    image_index = 0  # Remplacez 0 par l'indice de l'image que vous souhaitez utiliser
+    image = x_test[image_index]
+    label = y_test[image_index]
+
+    # Prétraitement de l'image pour qu'elle soit dans le bon format pour le modèle (déjà présent dans votre code)
+    image = np.expand_dims(image, axis=0)
+    image = np.expand_dims(image, axis=-1)
+
+    # Prédire le label de l'image en utilisant votre modèle
+    prediction = model.predict(image)
+    predicted_label = np.argmax(prediction)
+
+    # Afficher l'image avec le label prédit
+    plt.imshow(image.squeeze(), cmap='gray')
+    plt.title(f"Label réel : {label}, Label prédit : {predicted_label}")
+    plt.axis('off')
+    plt.show()
+
+if __name__ == "__main__" : 
+    ds_train, ds_val, ds_test = get_dataset_keras()
+    model = create_model()
+    train = train(model, ds_train, ds_val)
+    visualisation(model, train, ds_train, ds_val, ds_test)
