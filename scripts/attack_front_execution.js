a3baacd8bfb404fd04c66cf304a482976672dc9a
Gabriel Farago <38406378+Gimligili@users.noreply.github.com>
2024-03-29 23:22:20 +0100

    Solving visibility issue with spinner and improving ergonomics of the attackPanel

--- scripts/attack_front_execution.js
+++ scripts/attack_front_execution.js
@@ -3,11 +3,21 @@ const outputContainer = document.getElementById('outputContainer');
 const spinner = document.getElementById('spinner');
 let eventSource;
 
+
+
+window.onload = function() {
+    var executeBtnWidth = document.getElementById("executeBtn").offsetWidth;
+    document.getElementById("captchaTypeSelect").style.width = executeBtnWidth + "px";
+    var executeBtnHeight = document.getElementById("executeBtn").offsetHeight;
+    document.getElementById("captchaTypeSelect").style.height = executeBtnHeight + "px";
+};
+
+
 executeBtn.addEventListener('click', () => {
     const captchaType = document.getElementById('captchaTypeSelect').value;
     // Clear output container
     outputContainer.innerText = 'Executing Python code...\n';
-    spinner.style.display = 'block';
+    spinner.style.visibility = 'visible';
 
     // Close the existing event source if it exists
     if (eventSource) {
@@ -20,22 +30,25 @@ executeBtn.addEventListener('click', () => {
 
     // Event listener for receiving messages from server
     eventSource.onmessage = function(event) {
-        // Append received data to output container
-        outputContainer.innerText += event.data + '\n';
-        // Automatically scroll to the bottom of the container
-        outputContainer.scrollTop = outputContainer.scrollHeight;
-
         if (event.data.trim() === 'End of data') {
             // Close the event source
             eventSource.close();
+            // Hide spinner
+            spinner.style.visibility = 'hidden';
         }
+        else {
+            // Append received data to output container
+            outputContainer.innerText += event.data + '\n';
+            // Automatically scroll to the bottom of the container
+            outputContainer.scrollTop = outputContainer.scrollHeight;   
+        }     
     };
 
     // Event listener for when the connection is closed
     eventSource.onclose = function() {
         console.log('Connection closed.');
         // Hide spinner
-        spinner.style.display = 'none';
+        spinner.style.visibility = 'hidden';
     };
 
     // Event listener for errors
