c0b9bddbce153a61438d8a1f0b44109ecd9df4b1
Gabriel Farago <38406378+Gimligili@users.noreply.github.com>
2024-04-11 14:07:18 +0200

    Working on python code to have functionnal model metrics output

--- attack_utils/metrics.py
+++ attack_utils/metrics.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 import tensorflow_datasets as tfds
 from dl_models import labeler_cnn_mnist
+from dl_models import attacker_emnist
 
 
 
@@ -52,28 +53,30 @@ def output_metrics(CaptchaType, model):
 
         # Generating custom EMNIST test dataset
         # Directory containing the EMNIST images organized by labels
-        data_dir = os.getcwd() + '/images_dirs/'
-
-        batch_size = 2
-        img_height = 28
-        img_width = 28
-
+        # data_dir = os.getcwd() + '/images_dirs/'
 
         # Create a TensorFlow dataset
-        ds_emnist = tf.keras.utils.image_dataset_from_directory(data_dir,color_mode='grayscale',seed=123,image_size=(img_height, img_width),batch_size=batch_size)
-
+       
+        _, _, ds_test = attacker_emnist.get_dataset_keras()
 
         # Set up model and evaluate similar to MNIST
-        loss, accuracy = model.evaluate(ds_emnist, verbose=2)
+        loss, accuracy = model.evaluate(ds_test, verbose=2)
 
+        # Print evaluation metrics
+        print("\nEvaluation Metrics:")
+        print("Loss: {:.4f}".format(loss))
+        print("Accuracy: {:.2f}%".format(accuracy * 100))
 
-
-
-        #model = tf.keras.models.load_model(checkpoint_path)
-        # _, _, ds_test = attacker_emnist.get_dataset_keras()
-        #ds_test = attacker_emnist.get_test_dataset_kaggle()
-        # loss, acc = model.evaluate(ds_test, verbose=2)
-        # print("Restored model, accuracy: {:5.2f}%".format(100 * acc))
+        # Additional Information
+        print("\nAdditional Information:")
+        print("Number of trainable parameters: {}".format(
+            sum([tf.keras.backend.count_params(w) for w in model.trainable_weights])))
+        print("Number of non-trainable parameters: {}".format(
+            sum([tf.keras.backend.count_params(w) for w in model.non_trainable_weights])))
+        print("Input shape: {}".format(model.input_shape))
+        print("Output shape: {}".format(model.output_shape))
+        print("Optimizer: {}".format(model.optimizer.get_config()))
+        print("Learning rate: {}".format(model.optimizer.learning_rate.numpy()))
     
     #Model PYTHON
     elif CaptchaType == 2:
@@ -106,20 +109,4 @@ def output_metrics(CaptchaType, model):
         print("Output shape: {}".format(model.output_shape))
         print("Optimizer: {}".format(model.optimizer.get_config()))
         print("Learning rate: {}".format(model.optimizer.learning_rate.numpy()))
-
-
-
-
-
-def label_to_chr_emnist(arr) : 
-    result = []
-    for elt in arr : 
-        if elt <= 9 : 
-            result.append(elt.numpy())
-        elif elt < 36 : 
-            result.append(chr(elt - 10 + ord('A')))
-        else : 
-            result.append(chr(elt - 36 + ord('a')))
-    return result
-
 
