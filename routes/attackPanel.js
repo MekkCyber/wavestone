96c9288be22d3797bb7bdb000581cbd426fa1fd8
Gabriel Farago <38406378+Gimligili@users.noreply.github.com>
2024-04-19 05:29:19 +0200

    Removing unused code (no loss)

--- routes/attackPanel.js
+++ routes/attackPanel.js
@@ -34,16 +34,6 @@ function launchAttack(req, res, captchaType, iteration, lr, debug) {
         });
     }
 
-    // pythonProcess.stderr.on('data', (data) => {
-    //     console.log(data)
-    //     buffer += data.toString(); // Append data to buffer
-    //     const lines = buffer.split('\n'); // Split buffer into lines
-    //     buffer = lines.pop(); // Update buffer with incomplete line
-    //     lines.forEach((line) => {
-    //         res.write(`data: ${line}\n\n`); // Send each line as Server-Sent Event
-    //     });
-    // });
-
     // Handle Python process close event
     pythonProcess.on('close', (code) => {
         console.log(`Python script execution finished with code ${code}`);
